f2791a9145e8eec95ae66fb9f06c9cd2b36c8ac1
yusufElnady <yusufelnady3@gmail.com>
2022-08-22 01:32:45 +0200

    uploaded the initial code

---
+++ printing_names.py
@@ -0,0 +1,29 @@
+'''
+call the function with your name as a parameter (string)
+then run the code to see how your name will be printed
+reminder:
+1- git add .
+2- git commit -m "any message"
+3- git push
+'''
+import string
+
+s = "闩⻏⼕ᗪ🝗ﾁᎶ卄讠丿长㇄爪𝓝ㄖ尸Ɋ尺丂〸ㄩᐯ山〤丫Ⲍ"
+alphabet = {}
+
+for i in range(len(string.ascii_lowercase)):
+	alphabet[string.ascii_lowercase[i]] = s[i]
+
+def print_name(name):
+	for char in name:
+		char = char.lower()
+		if char in alphabet:
+			print(alphabet[char], end="")
+		else:
+			print(char, end="")
+	print()
+
+print_name("Yusuf el-nady")
+print_name("Youssef Saeed")
+print_name("Shahd Tharwat")
+print_name("Hana Ghanem")
